1b7022ee146ddcfde9d054c82e02bff91cec93ba
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-04-20 12:07:44 -0500

    Added addNewSmurf function for posting new smurs

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,27 @@
 import axios from 'axios';
 
-export const ADD_SMURF = 'ADD_SMURF'
+export const FETCH_SMURFS_START = 'FETCH_SMURFS_START'
 export const FETCH_SMURFS_SUCCESS = 'FETCH_SMURFS_SUCCESS'
 export const FETCH_SMURFS_FAILURE = 'FETCH_SMURFS_FAILURE'
 
+export const POST_SMURF_START = 'POST_SMURF_START'
+export const POST_SMURF_SUCCESS = 'POST_SMURF_SUCCESS'
+export const POST_SMURF_FAILURE = 'POST_SMURF_FAILURE'
+
 export const fetchSmurfList = () => dispatch => {
-  dispatch({ type: FETCH_SMURFS_SUCCESS })
+  dispatch({ type: FETCH_SMURFS_START })
   axios
     .get('http://localhost:3333/smurfs')
     .then(res => dispatch({ type: FETCH_SMURFS_SUCCESS, payload: console.log(res) }))
     .catch( err => dispatch ({ type: FETCH_SMURFS_FAILURE, payload: console.log(err) }))
+}
+
+export const addNewSmurf = () => dispatch => {
+    dispatch({ type: POST_SMURF_START })
+    axios
+      .post('http://localhost:3333/smurfs', payload)
+      .then(res => dispatch({ type: POST_SMURF_SUCCESS, payload: res.data }))
+      .catch(err => dispatch({ type: POST_SMURF_FAILURE, payload: err }));
 }
 
 
